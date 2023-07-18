dbf300ba8c191a9601e8dd60bc7ac665e84d286e
csimpf <30953800+csimpf@users.noreply.github.com>
2023-07-18 14:04:13 +0000

    Move getKeep into directory

--- google-keep-get/getKeep.py
+++ google-keep-get/getKeep.py
@@ -3,7 +3,7 @@ import os
 import datetime
 from dotenv import load_dotenv
 
-sys.path.append('lib/gkeepapi')
+sys.path.append('../lib/gkeepapi')
 import gkeepapi
 
 load_dotenv()
@@ -13,13 +13,17 @@ success = keep.login(os.environ["GOOGLE_USERNAME"], os.environ["GOOGLE_APP_PASSW
 print("Login success? ", success)
 
 # keep.find returns a generator - use next(gnotes) to get the first in the list
-gnotes = keep.find(query='Shopping')
+gnotes = keep.find(query=os.environ["KEEP_LIST_NAME"])
 
 shopping_note = next(gnotes)
 
 curr_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-shopping_note.text = shopping_note.text + "\n Last edit:"+ curr_date
+# shopping_note.text = shopping_note.text + "\n Last update:"+ curr_date
+print(shopping_note.text)
 
+item_list = shopping_note.text[:shopping_note.text.index("Last update:")].split('\n')[:-1]
+
+# send item_list to lambda to update AnyList
 keep.sync()
-print("sync success?")
+print(item_list)
